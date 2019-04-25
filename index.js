30ee77345d6792bfde99aead722d74862dfcb2bc
Karima El Hachimi <karima.elhachimi@hotmail.com>
2019-04-25 16:00:57 +0200

    port and host fix for heroku

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const fs = require('fs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const app = express();
 
 
-app.listen(port, () => console.log(`app is running on localhost:${port}`));
+app.get('/apics/lock/:lockId', (req, res) => {
+
+    const lock = readJsonFromFile('./stubs/lock.json');
+    res.json(lock);
+
+});
+
+app.get('/apics/locks', (req, res) => {
+
+    const lock = readJsonFromFile('./stubs/locks.json');
+    res.json(lock);
+
+});
+
+app.get('/apics/lockexecutions/:lockCode', (req, res) => {
+
+    const lock = readJsonFromFile('./stubs/lockExecutions.json');
+    res.json(lock);
+
+});
+
+app.get('/apics/lockexecution/:lockid', (req, res) => {
+
+    const lock = readJsonFromFile('./stubs/lockExecution.json');
+    res.json(lock);
+
+});
+
+function readJsonFromFile(fileLocation) {
+    const rawData = fs.readFileSync(fileLocation);
+    const json = JSON.parse(rawData);
+    console.log(`parsed file in json: ${json}`);
+    return json;
+}
+app.listen(port, '0.0.0.0', () => console.log(`app is running on localhost:${port}`));
